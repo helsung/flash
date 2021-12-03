0585e1ca571ae67ae392602d32186dd93e9163b2
Helen Sung <helenjeehosung@gmail.com>
2021-12-03 00:06:21 -0500

    feat: fetch and SSR curated photos

---
+++ components/Photos.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "../styles/Photos.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Photos({ photos }) {
+  return (
+    <div className={styles.gallery}>
+      {photos.map((photo) => (
+        <div key={photo.id}>
+          <Image
+            src={photo.src.large}
+            width={600}
+            height={900}
+            alt="single-photo"
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
