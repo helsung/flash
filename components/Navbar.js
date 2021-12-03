f483db45bd8d8f5dfffc56dce8235fb555224e9a
Helen Sung <helenjeehosung@gmail.com>
2021-12-02 23:15:21 -0500

    feat: navbar handling user search input

---
+++ components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "../styles/Navbar.module.css";
+import Image from "next/image";
+import logo from "./,,/../../public/logo.png";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function Navbar({ input, handleInput, refreshData }) {
+  const router = useRouter();
+  return (
+    <div className={styles.container}>
+      <Image src={logo} alt="logo" onClick={() => router.push(`/${input}`)} />
+
+      <input
+        className={styles.inputField}
+        placeholder="Search for photos"
+        value={input}
+        onChange={(e) => handleInput(e.target.value)}
+      />
+      <Link href={`/${input}`} passHref>
+        <button onClick={refreshData}>Search</button>
+      </Link>
+    </div>
+  );
+}
