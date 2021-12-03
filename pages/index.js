91f4d943ffff7f43d589db84601ae5662ec56a36
Helen Sung <helenjeehosung@gmail.com>
2021-12-03 01:40:20 -0500

    feat: pagination on curated photos

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar";
 import Photos from "../components/Photos";
 import { getCuratedPhotos } from "../lib/photos";
+import Pagination from "next-pagination";
+import { useRouter } from "next/router";
 
 export default function Home({ curatedPhotos }) {
-  const [photos, setPhotos] = useState(curatedPhotos.photos);
+  const { photos } = curatedPhotos;
   const [input, setInput] = useState("");
+  const router = useRouter();
+  const totalResults = (curatedPhotos && curatedPhotos.total_results) || 8000;
+  const perPage = router.query.size || 20;
+  const totalPages = Math.ceil(totalResults / perPage);
+
+  useEffect(() => {
+    router.replace("/?page=1&size=20");
+  }, []);
 
   const handleInput = (e) => {
     setInput(e);
@@ -16,12 +26,14 @@ export default function Home({ curatedPhotos }) {
     <div className={styles.container}>
       <Navbar input={input} handleInput={handleInput} />
       <Photos photos={photos} />
+      <Pagination total={totalPages} />
     </div>
   );
 }
 
-export async function getServerSideProps({}) {
-  const curatedPhotos = await getCuratedPhotos();
+export async function getServerSideProps({ query }) {
+  const { page, size } = query;
+  const curatedPhotos = await getCuratedPhotos(+page, +size);
 
   return {
     props: {
